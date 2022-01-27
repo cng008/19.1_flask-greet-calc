2c2cacd00029da519718cd712fa394c392186d26
Christien Ng <ngchristien@gmail.com>
2022-01-26 21:43:11 -0800

    condense to one route

--- calc/app.py
+++ calc/app.py
@@ -3,43 +3,19 @@ from operations import add, sub, mult, div
 
 app = Flask(__name__)
 
-@app.route('/add')
-def to_add():
-    """ Add a and b params """
+operators = {
+    "add": add,
+    "sub": sub,
+    "mult": mult,
+    "div": div
+}
+
+@app.route('/math/<op>')
+def math(op):
+    """ do some operation on a and b params """
 
     a = int(request.args.get('a'))
     b = int(request.args.get('b'))
-    res = add(a, b)
+    res = operators[op](a, b)
 
-    return str(res)
-
-@app.route('/sub')
-def to_sub():
-    """ Subtract a and b params """
-
-    a = int(request.args.get('a'))
-    b = int(request.args.get('b'))
-    res = sub(a, b)
-    
-    return str(res)
-
-@app.route('/mult')
-def to_mult():
-    """ Multiply a and b params """
-
-    a = int(request.args.get('a'))
-    b = int(request.args.get('b'))
-    res = mult(a, b)
-    
-    return str(res)
-
-
-@app.route('/div')
-def to_div():
-    """ Divide a and b params """
-
-    a = int(request.args.get('a'))
-    b = int(request.args.get('b'))
-    res = div(a, b)
-    
     return str(res)
